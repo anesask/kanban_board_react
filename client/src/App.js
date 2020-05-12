8345dee89c683a543ce9dcfd6ad5ef8ceb62c663
Anes Mulalic <anes.mulalic@hotmail.com>
2020-05-12 20:31:26 +0200

    Screenshot

--- client/src/App.js
+++ client/src/App.js
@@ -10,25 +10,26 @@ function App() {
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="section bg-full">
         <div className="container bg-blue">
-        
-        <div className="columns is-multiline is-mobile">
-          {state.columnsOrder.map((columnId, index) => {
-            const column = state.columns[columnId];
-            const isEditing = column.isEditing;
-            const task = column.tasksOrder.map((taskId) => state.tasks[taskId]);
-            return (
-              <Column
-                key={column.id}
-                tasks={task}
-                index={index}
-                column={column}
-                isEditing={isEditing}
-              />
-            );
-          })}
+          <div className="columns is-multiline is-mobile">
+            {state.columnsOrder.map((columnId, index) => {
+              const column = state.columns[columnId];
+              const isEditing = column.isEditing;
+              const task = column.tasksOrder.map(
+                (taskId) => state.tasks[taskId]
+              );
+              return (
+                <Column
+                  key={column.id}
+                  tasks={task}
+                  index={index}
+                  column={column}
+                  isEditing={isEditing}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
-        </div>
     </DragDropContext>
   );
 }
